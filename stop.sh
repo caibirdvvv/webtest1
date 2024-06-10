9d379b1393c50dc76d256f1d539da591ff6d5a38
TIM <TIM@COOK.com>
2024-06-10 10:42:47 +0800

    添加uwsgi脚本文件

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/user/bin/env bash
+
+echo -e "\033[34m---------------------wsgi process---------------------\033[0m"
+
+ps -ef|grep v1_uwsgi.ini | grep -v grep
+
+sleep 0.5
+
+echo -e '\n-------------------------going to close-------------------------'
+
+ps -ef | grep v1_uwsgi.ini | grep -v grep | awk '{print $2}' | xargs kill -9
+
+sleep 0.5
